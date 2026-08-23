1a3831145362f6f63d18d051ecf606bfbf579457
roddaniels <rodbagares.au@gmail.com>
2026-08-23 11:00:39 +1000

    updated projects

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { ButtonModule } from 'primeng/button';
+import { ChipModule } from 'primeng/chip';
 
 
 @Component({
   selector: 'app-projects',
   imports: [
     MatCardModule,
-    ButtonModule
+    ButtonModule,
+    ChipModule
   ],
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
